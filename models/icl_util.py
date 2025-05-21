356b3436cbec504087cd7fccb1308025624d2a97
William Convertino <williamconvertino@gmail.com>
2025-05-21 14:46:34 -0400

    Added b and c models

--- models/icl_util.py
+++ models/icl_util.py
@@ -4,6 +4,99 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchtune.modules import RotaryPositionalEmbeddings
 
+
+class ICLAttentionC(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        
+        self.W_q = nn.Parameter(torch.zeros(config.n_heads, config.d_embed, config.d_hidden), requires_grad=True)
+        self.W_k = nn.Parameter(torch.zeros(config.n_heads, config.d_embed, config.d_hidden), requires_grad=True)
+        self.W_v = nn.Parameter(torch.zeros(config.n_heads, config.d_embed, config.d_hidden), requires_grad=True)
+        self.W_o = nn.Parameter(torch.zeros(config.n_heads * config.d_embed, config.d_embed), requires_grad=True)
+
+        N_scaling = 1.0 / torch.arange(1, config.max_seq_len + 1).float() # (S)
+        self.register_buffer("N_scaling", N_scaling.view(1, config.max_seq_len, 1)) # (1, S, 1) 
+
+        self.rotary_embeddings = RotaryPositionalEmbeddings(config.d_embed)
+        
+        self.drop_attn = nn.Dropout(0.1)
+        self.drop_resid = nn.Dropout(0.1)
+        
+    def forward(self, q, k, v):
+        
+        B, S, E = q.shape
+        
+        q = torch.einsum('bsz,hzd->bhsd', q, self.W_q) # (B, H, S, D)
+        k = torch.einsum('bsz,hzd->bhsd', k, self.W_k) # (B, H, S, D)
+        v = torch.einsum('bsz,hzd->bhsd', v, self.W_v) # (B, H, S, D)
+        
+        q = self.rotary_embeddings(q) # (B, H, S, E)
+        k = self.rotary_embeddings(k) # (B, H, S, E)
+
+        causal_mask = torch.triu(torch.ones(S, S), diagonal=0).bool().to(q.device) # (S, S)
+        causal_mask[0, 0] = False # Ignore the first token (Fixes softmax)
+        
+        attn_scores = torch.einsum('bhqd,bhkd->bhqk', q, k) # (B, H, S, S)
+        attn_scores = attn_scores.masked_fill(causal_mask, float('-inf')) # (B, H, S, S)
+        
+        attn_probs = F.softmax(attn_scores, dim=-1) # (B, H, S, S)
+        attn_probs = self.drop_attn(attn_probs) # (B, H, S, S)
+        
+        attn_output = torch.einsum('bhzs,bhzd->bhsd', attn_probs, v) # (B, H, S, E)
+        attn_output = attn_output.transpose(1, 2).contiguous().view(B, S, self.config.d_hidden * self.config.n_heads) # (B, S, H * E)
+
+        attn_output = torch.einsum('bsz,zd->bsd', attn_output, self.W_o) # (B, S, D)
+
+        return attn_output
+
+class ICLAttentionB(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        
+        self.W_q = nn.Parameter(torch.zeros(config.n_heads, config.d_embed, config.d_hidden), requires_grad=True)
+        self.W_k = nn.Parameter(torch.zeros(config.n_heads, config.d_embed, config.d_hidden), requires_grad=True)
+        self.W_o = nn.Parameter(torch.zeros(config.n_heads * config.d_embed, config.d_embed), requires_grad=True)
+
+        N_scaling = 1.0 / torch.arange(1, config.max_seq_len + 1).float() # (S)
+        self.register_buffer("N_scaling", N_scaling.view(1, config.max_seq_len, 1)) # (1, S, 1) 
+
+        self.rotary_embeddings = RotaryPositionalEmbeddings(config.d_embed)
+        
+        self.drop_attn = nn.Dropout(0.1)
+        self.drop_resid = nn.Dropout(0.1)
+        
+    def forward(self, q, k, v):
+        
+        B, S, E = q.shape
+
+        v = v.unsqueeze(1).expand(-1, self.config.n_heads, -1, -1) # (B, H, S, E)
+        
+        q = torch.einsum('bsz,hzd->bhsd', q, self.W_q) # (B, H, S, D)
+        k = torch.einsum('bsz,hzd->bhsd', k, self.W_k) # (B, H, S, D)
+        
+        q = self.rotary_embeddings(q) # (B, H, S, E)
+        k = self.rotary_embeddings(k) # (B, H, S, E)
+
+        causal_mask = torch.triu(torch.ones(S, S), diagonal=0).bool().to(q.device) # (S, S)
+        causal_mask[0, 0] = False # Ignore the first token (Fixes softmax)
+        
+        attn_scores = torch.einsum('bhqd,bhkd->bhqk', q, k) # (B, H, S, S)
+        attn_scores = attn_scores.masked_fill(causal_mask, float('-inf')) # (B, H, S, S)
+        
+        attn_probs = F.softmax(attn_scores, dim=-1) # (B, H, S, S)
+        attn_probs = self.drop_attn(attn_probs) # (B, H, S, S)
+        
+        attn_output = torch.einsum('bhzs,bhzd->bhsd', attn_probs, v) # (B, H, S, E)
+        attn_output = attn_output.transpose(1, 2).contiguous().view(B, S, self.config.d_hidden * self.config.n_heads) # (B, S, H * E)
+
+        attn_output = torch.einsum('bsz,zd->bsd', attn_output, self.W_o) # (B, S, D)
+
+        return attn_output
+    
 class ICLAttentionA(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -110,6 +203,10 @@ class ICLBlock(nn.Module):
             self.attn = ICLAttentionExact(config)
         elif self.config.icl_mode == 'a':
             self.attn = ICLAttentionA(config)
+        elif self.config.icl_mode == 'b':
+            self.attn = ICLAttentionB(config)
+        elif self.config.icl_mode == 'c':
+            self.attn = ICLAttentionC(config)
         
     # NOTE: Only works for single layer atm (assumes functional_update is zero)
     def calculate_embedding_expectation(self, functional_update): 
