Fixed causal mask

models/transformer_util.py
@@ -38,7 +38,7 @@ class Attention(nn.Module):
         q = self.rotary_embeddings(q) # (B, H, S, D)
         k = self.rotary_embeddings(k) # (B, H, S, D)
         
-        causal_mask = torch.triu(torch.ones(S, S), diagonal=1).bool().to(q.device) # (S, S)
+        causal_mask = torch.triu(torch.ones(S, S), diagonal=0).bool().to(q.device) # (S, S)
         
         attn_scores = torch.einsum('bhqd,bhkd->bhqk', q, k) # (B, H, S, S)
         attn_scores = attn_scores * self.attn_scale # (B, H, S, S)